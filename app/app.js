bcec5763c55d8e010a809ac4957e93170a733c03
yangbin <yangbin@xueqiu.com>
2013-08-20 23:14:55 +0800

    init file

---
+++ app/app.js
@@ -0,0 +1,50 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , config = require("./config")();
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hjs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+console.log(config)
+MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/fastdelivery', function(err, db) {
+    if(err) {
+        console.log('Sorry, there is no mongo db server running.');
+    } else {
+        var attachDB = function(req, res, next) {
+            req.db = db;
+            next();
+        };
+        http.createServer(app).listen(config.port, function(){
+            console.log('Express server listening on port ' + config.port);
+        });
+    }
+});
---
+++ app/config/mongo.js
@@ -0,0 +1,9 @@
+var config = {
+    mongo:{
+        host: "127.0.0.1",
+        port: 27017
+    }
+}
+module.exports = function(mode) {
+    return config
+}
